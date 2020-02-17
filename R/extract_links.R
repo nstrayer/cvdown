418c38cfccc252147bf73f2b99d794f9e07b8f40
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:32:20 -0600

    Added namespacing for external package functions to extract_links

--- R/extract_links.R
+++ R/extract_links.R
@@ -17,30 +17,33 @@
 extract_links <- function(text, cv){
 
   # Regex to locate links in text
-  find_link <- regex("
-  \\[   # Grab opening square bracket
-  .+?   # Find smallest internal text as possible
-  \\]   # Closing square bracket
-  \\(   # Opening parenthesis
-  .+?   # Link text, again as small as possible
-  \\)   # Closing parenthesis
-  ", comments = TRUE)
+  find_link <- stringr::regex("
+    \\[   # Grab opening square bracket
+    .+?   # Find smallest internal text as possible
+    \\]   # Closing square bracket
+    \\(   # Opening parenthesis
+    .+?   # Link text, again as small as possible
+    \\)   # Closing parenthesis
+    ", comments = TRUE)
 
   links <- c()
   num_links_prev <- length(attr(cv, 'links'))
-  str_extract_all(text, find_link) %>%
-    pluck(1) %>%
-    walk(function(link_from_text){
-      title <- link_from_text %>% str_extract('\\[.+\\]') %>% str_remove_all('\\[|\\]')
-      link <- link_from_text %>% str_extract('\\(.+\\)') %>% str_remove_all('\\(|\\)')
+
+  found_links <- stringr::str_extract_all(text, find_link)[[1]]
+
+  purrr::walk(
+    found_links,
+    function(link_from_text){
+      title <- link_from_text %>% stringr::str_extract('\\[.+\\]') %>% stringr::str_remove_all('\\[|\\]')
+      link <- link_from_text %>% stringr::str_extract('\\(.+\\)') %>% stringr::str_remove_all('\\(|\\)')
 
       footnote_number <- num_links_prev + length(links) + 1
 
       # Build replacement text
-      new_text <- glue('{title}<sup>{footnote_number}</sup>')
+      new_text <- glue::glue('{title}<sup>{footnote_number}</sup>')
 
       # Replace text
-      text <<- text %>% str_replace(fixed(link_from_text), new_text)
+      text <<- text %>% stringr::str_replace(stringr::fixed(link_from_text), new_text)
 
       links <<- c(links, link)
     })
