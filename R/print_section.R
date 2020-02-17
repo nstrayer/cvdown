21aaea30e77a6f1e24af082f4f2fbc55f83c25c8
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 14:41:29 -0600

    print_section method now prints the header for the section along with any desired aside text meaning most of the CV can just be one long pipe.

--- R/print_section.R
+++ R/print_section.R
@@ -2,6 +2,9 @@
 #'
 #' @inheritParams print_skill_bars
 #' @param section_id Name of section to be printed as encoded by the column `section` in positions dataframe.
+#' @param title Name to be printing at top of section. Defaults to `section_id` value.
+#' @param icon Name of font-awesome icon to be used for section.
+#' @param aside_text Name of label for aside text as stored in the `text_blocks` section of cv data. Leave as `NULL` to ommit.
 #'
 #' @return
 #' @export
@@ -23,17 +26,34 @@
 #' # Print education section
 #' printer <- print_section(printer, 'education')
 #'
-print_section <- function(cv, section_id){
+print_section <- function(cv, section_id, title = section_id, icon = "graduation-cap", aside_text = NULL){
 
   UseMethod("print_section")
 }
 
 #' @export
-print_section.cv_printer <- function(cv, section_id){
+print_section.cv_printer <- function(cv, section_id, title = section_id, icon = "graduation-cap", aside_text = NULL){
 
   if(is.null(cv$positions)){
     stop("Missing position data. Make sure when you setup your cv printer your passed data has position data stored with list key of 'positions'.")
   }
+
+  section_sep <- paste(rep('-',80), collapse = "")
+  glue::glue("{title} {{data-icon={icon}}}",
+             "{section_sep}",
+             "",
+             .sep = "\n") %>%
+    cat("\n")
+
+
+  # If the user has requested aside text for the is section, print if before starting main content
+  has_aside_text <- !is.null(aside_text)
+  if(has_aside_text){
+    cat("\n::: aside", "\n")
+    cv <- cv %>% print_text_block(aside_text)
+    cat("\n:::\n")
+  }
+
 
   data_for_position <- cv$positions %>%
     dplyr::filter(section == section_id)
