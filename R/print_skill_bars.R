fe2966427b99f696c7b7bd09bac1e731b3a9a81a
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:32:39 -0600

    Added namespacing and example code for print_skill_bars method

--- R/print_skill_bars.R
+++ R/print_skill_bars.R
@@ -11,20 +11,40 @@
 #' @export
 #'
 #' @examples
+#'
+#' # Function to get sample csvs from package data store.
+#' sample_data <- function(name) system.file(paste0('sample_csvs/', name), package = "cvdown")
+#'
+#' # Run function on these sample datasets
+#' data <- gather_data_from_csvs(positions_loc = sample_data('positions.csv'),
+#'                               skills_loc = sample_data('language_skills.csv'),
+#'                               text_blocks_loc = sample_data('text_blocks.csv'),
+#'                               contact_info_loc = sample_data('contact_info.csv'))
+#'
+#' # Setup CV printer
+#' printer <- new_cv_printer(data, pdf_mode = TRUE)
+#'
+#' # Print skill bars
+#' printer <- print_skill_bars(printer)
+#'
+#'
 print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9"){
 
   UseMethod("print_skill_bars")
 }
 
 #' @export
-print_skill_bars.cv_printer <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9"){
+print_skill_bars.cv_printer <- function(cv,
+                                        out_of = 5,
+                                        bar_color = "#969696",
+                                        bar_background = "#d9d9d9"){
   if(is.null(cv$skills)){
     stop("Missing skills data. Make sure when you setup your cv printer your passed data has skills data stored with list key of 'skills'.")
   }
 
   cv$skills %>%
-    mutate(width_percent = round(100*level/out_of)) %>%
-    glue_data(
+    dplyr::mutate(width_percent = round(100*level/out_of)) %>%
+    glue::glue_data(
       "<div class = 'skill-bar'",
       "style = \"background:linear-gradient(to right,",
       "{bar_color} {width_percent}%,",
