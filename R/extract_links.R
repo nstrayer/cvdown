4f8362ec99f3eafcf009d3c019eaf99ed02d1fea
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:44:04 -0600

    Added example usage of extract_links helper function

--- R/extract_links.R
+++ R/extract_links.R
@@ -14,6 +14,29 @@
 #' @export
 #'
 #' @examples
+#'
+#' text_w_links <- "Here is my text with a cool [markdown link](https://my_cool_website.com/a_page) in it. For good measure [here's another link.](http://another_cool_site.com/)"
+#'
+#' # Setup empty CV printer
+#' printer <- new_cv_printer(list(), pdf_mode = TRUE)
+#'
+#' extract_results <- extract_links(text_w_links, printer)
+#'
+#' # Text w/ no links is here
+#' extract_results$text
+#'
+#' # Extracted links are here
+#' extract_results$links
+#'
+#' # No links stored yet
+#' attr(printer, 'links')
+#'
+#' # We can then append these links to our CV so it remembers them for later footnote printing
+#' printer <- printer %>% append_links(extract_results$links)
+#'
+#' # Links have been updated
+#' attr(printer, 'links')
+#'
 extract_links <- function(text, cv){
 
   # Regex to locate links in text
