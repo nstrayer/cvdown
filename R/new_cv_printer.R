9955720b61c1461690a6aeb75b7d4a1119d803e8
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:37:41 -0600

    Added example code to initialization function

--- R/new_cv_printer.R
+++ R/new_cv_printer.R
@@ -8,6 +8,22 @@
 #' @export
 #'
 #' @examples
+#'
+#' # Function to get sample csvs from package data store.
+#' sample_data <- function(name) system.file(paste0('sample_csvs/', name), package = "cvdown")
+#'
+#' # Run function on these sample datasets
+#' data <- gather_data_from_csvs(positions_loc = sample_data('positions.csv'),
+#'                               skills_loc = sample_data('language_skills.csv'),
+#'                               text_blocks_loc = sample_data('text_blocks.csv'),
+#'                               contact_info_loc = sample_data('contact_info.csv'))
+#'
+#' # Setup CV printer
+#' printer <- new_cv_printer(data, pdf_mode = TRUE)
+#'
+#' # Print contact info
+#' printer <- print_contact_info(printer)
+#'
 new_cv_printer <- function(cv_data, pdf_mode = TRUE){
 
   structure(
