ab6846594d33581b1fcc67e043723ee434613956
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:05:37 -0600

    Added roxygen to new_cv_printer function

--- R/new_cv_printer.R
+++ R/new_cv_printer.R
@@ -1,5 +1,14 @@
+#' Setup CV printer object
+#'
+#' @param cv_data List of dataframes containging info for CV
+#' @param pdf_mode Boolean of if document is being exported as pdf and thus
+#'   links should be stripped for placement at end.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 new_cv_printer <- function(cv_data, pdf_mode = TRUE){
-
 
   structure(
     cv_data,
