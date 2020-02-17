ef4f279a9d50fc1342358ee0adf3c4c32b911d56
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:05:03 -0600

    Added roxygen to print_skill_bars

--- R/print_skill_bars.R
+++ R/print_skill_bars.R
@@ -1,4 +1,18 @@
+#' Print skill bars
+#'
+#' Writes out the skill bars as divs with varying widths based on skill level
+#'
+#' @param cv object of class `cv_printer` created by \code{\link{new_cv_printer}}
+#' @param out_of What is the skill value out of?
+#' @param bar_color Color of filled in skill bar
+#' @param bar_background Color of background (unfilled in part) of skill bar.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9"){
+
   UseMethod("print_skill_bars")
 }
 
