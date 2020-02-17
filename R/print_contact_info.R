979187970bb547b3b87be759b6306f1a7e3b58af
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 13:36:37 -0600

    Added namespacing and example code to print_contact_info method

--- R/print_contact_info.R
+++ R/print_contact_info.R
@@ -8,6 +8,23 @@
 #' @export
 #'
 #' @examples
+#'
+#' # Function to get sample csvs from package data store.
+#' sample_data <- function(name) system.file(paste0('sample_csvs/', name), package = "cvdown")
+#'
+#' # Run function on these sample datasets
+#' data <- gather_data_from_csvs(positions_loc = sample_data('positions.csv'),
+#'                               skills_loc = sample_data('language_skills.csv'),
+#'                               text_blocks_loc = sample_data('text_blocks.csv'),
+#'                               contact_info_loc = sample_data('contact_info.csv'))
+#'
+#' # Setup CV printer
+#' printer <- new_cv_printer(data, pdf_mode = TRUE)
+#'
+#' # Print contact info
+#' printer <- print_contact_info(printer)
+#'
+#'
 print_contact_info <- function(cv){
   UseMethod("print_contact_info")
 }
@@ -20,7 +37,7 @@ print_contact_info.cv_printer <- function(cv){
   }
 
   cv$contact_info %>%
-    glue_data("- <i class='fa fa-{icon}'></i> {contact}") %>%
+    glue::glue_data("- <i class='fa fa-{icon}'></i> {contact}") %>%
     cat(sep = "\n")
 
   cv
